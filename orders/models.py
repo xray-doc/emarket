7c84232b2d5fd02bb38775844853fcbb03e3d405
Mikhail Nikolaev <m.nikolaev1@gmail.com>
2017-12-10 18:37:21 +0300

    First commit

---
+++ orders/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from products.models import Product
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=64, blank=True, null=True, default=None)
+    is_active = models.BooleanField(default=True)
+
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "Статус %s" % self.name
+
+    class Meta:
+        verbose_name = "Статус"
+        verbose_name_plural = "Статусы заказа"
+
+
+
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=64, blank=True, null=True, default=None)
+    customer_email = models.EmailField(blank=True, null=True, default=None)
+    customer_phone = models.CharField(blank=True, null=True, default=None, max_length=48)
+    customer_address = models.CharField(blank=True, null=True, default=None, max_length=128)
+    comments = models.TextField(blank=True, null=True, default=None)
+    status = models.ForeignKey(Status)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "Заказ %s %s" % (self.id, self.status.name)
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+
+
+
+class ProductInOrder(models.Model):
+    order = models.ForeignKey(Order, blank=True, null=True, default=None)
+    product = models.ForeignKey(Product, blank=True, null=True, default=None)
+    nmb = models.IntegerField(default=1)
+    price_per_item = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "%s" % self.product.name
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
---
+++ products/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=64, blank=True, null=True, default=True)
+    description = models.TextField(blank=True, null=True, default=None)
+
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "%s" % self.name
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+
+
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to='products_images/')
+    product = models.ForeignKey(Product, blank=True, null=True, default=None)
+    is_active = models.BooleanField(default=True)
+
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "%s" % self.id
+
+    class Meta:
+        verbose_name = "Фотография"
+        verbose_name_plural = "Фотографии"
