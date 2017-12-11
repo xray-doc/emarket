2481bdf4553473860383a276e7f177ded5936074
Mikhail Nikolaev <m.nikolaev1@gmail.com>
2017-12-11 17:00:46 +0300

    Update orders/models.py and products/models.py

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from products.models import Product
+from django.db.models.signals import post_save
 
 
 class Status(models.Model):
@@ -46,7 +47,7 @@ class ProductInOrder(models.Model):
     nmb = models.IntegerField(default=1)
     price_per_item = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-
+    is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
@@ -54,6 +55,31 @@ class ProductInOrder(models.Model):
         return "%s" % self.product.name
 
     class Meta:
-        verbose_name = "Товар"
-        verbose_name_plural = "Товары"
+        verbose_name = "Товар в заказе"
+        verbose_name_plural = "Товары в заказе"
 
+
+    def save(self, *args, **kwargs):
+        price_per_item = self.product.price
+        self.price_per_item = price_per_item
+        self.total_price = self.nmb * price_per_item
+
+        super(ProductInOrder, self).save(*args, **kwargs)
+
+
+
+
+def product_in_order_post_save(sender, instance, created, **kwargs):
+    order = instance.order
+    all_products_in_order = ProductInOrder.objects.filter(order=order, is_active=True)
+
+    order_total_price = 0
+    for item in all_products_in_order:
+        order_total_price += item.total_price
+
+    instance.order.total_price = order_total_price
+    instance.order.save(force_update=True)
+
+
+
+post_save.connect(product_in_order_post_save, sender=ProductInOrder)
