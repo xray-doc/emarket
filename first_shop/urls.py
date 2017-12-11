da3f80e45a69d465077549ada62fd87e7ed156b3
Mikhail Nikolaev <m.nikolaev1@gmail.com>
2017-12-11 18:00:51 +0300

    Added first templates

--- first_shop/urls.py
+++ first_shop/urls.py
@@ -19,11 +19,13 @@ from django.conf.urls import include
 from django.contrib.staticfiles import *
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 
 
 
 urlpatterns = [
+    url(r'^', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^', include('orders.urls')),
     url(r'^', include('products.urls')),
