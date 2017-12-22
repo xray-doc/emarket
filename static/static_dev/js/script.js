e6921c5367114e6238fe5d5b0611e1c2b5371bb4
Mikhail Nikolaev <m.nikolaev1@gmail.com>
2017-12-22 22:32:21 +0300

    Update script.js

--- static/static_dev/js/script.js
+++ static/static_dev/js/script.js
@@ -1,17 +1,32 @@
+$(document).ready(function () {
+
+    // Появление списка корзины при наведении на кнопку "Корзина"
+
+    $("#basket-btn").mouseover(function (e) {
+        $(".basket").removeClass('hidden');
+    });
+    $('#basket-btn').mouseout(function (e) {
+        $(".basket").addClass('hidden')
+    });
 
 
-// Появление списка корзины при наведении на кнопку "Корзина"
+    // Добавление товара в список при нажатии кнопки "Добавить в корзину"
+
+    $('#form_buying_product').on('submit', function (e) {
+        e.preventDefault()
+        var nmb = $('#num-products-input').val()
+
+        var submit_btn = $('#submit-btn');
+        var product_name = submit_btn.data('name');
+        var product_price = submit_btn.data('price');
+
+        $('.container .basket ul').append('<li>'+product_name+', '+nmb+' шт., по '+product_price+'</li>')
+    })
 
 
-var bask_btn = document.getElementById("basket-btn");
+});
 
-bask_btn.onmouseover = function (e) {
-    $(".basket").removeClass('hidden');
-};
 
-bask_btn.onmouseout = function (e) {
-    $(".basket").addClass('hidden')
-};
 
 
 
