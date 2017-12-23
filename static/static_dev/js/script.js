0d95971417c65d32b554783f34b73cb96e5784c5
Mikhail Nikolaev <m.nikolaev1@gmail.com>
2017-12-23 18:11:30 +0300

    woking on basket list

--- static/static_dev/js/script.js
+++ static/static_dev/js/script.js
@@ -12,17 +12,27 @@ $(document).ready(function () {
 
     // Добавление товара в список при нажатии кнопки "Добавить в корзину"
 
+    var basketlist = []
     $('#form_buying_product').on('submit', function (e) {
-        e.preventDefault()
-        var nmb = $('#num-products-input').val()
+        e.preventDefault();
+        var nmb = $('#num-products-input').val();
 
         var submit_btn = $('#submit-btn');
         var product_name = submit_btn.data('name');
         var product_price = submit_btn.data('price');
+        
+        $('.container .basket ul').append('<li>'+product_name+', '+nmb+' шт., по '+product_price+' RUB ' +
+            '<a href="" class="delete-item">x</a>' +
+            '</li>')
+    });
 
-        $('.container .basket ul').append('<li>'+product_name+', '+nmb+' шт., по '+product_price+'</li>')
-    })
 
+    // Удаление из списка корзины
+
+    $(document).on('click', '.delete-item', function (e) {
+        e.preventDefault();
+        $(this).closest('li').remove();
+    });
 
 });
 
