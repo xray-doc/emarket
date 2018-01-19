f3278f64032a6262e55bdbf35dc61a7470db4d02
Mikhail Nikolaev <m.nikolaev1@gmail.com>
2018-01-19 17:02:00 +0300

    Fixed wsgi.py

--- first_shop/wsgi.py
+++ first_shop/wsgi.py
@@ -15,5 +15,5 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "first_shop.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = DjangoWhiteNoise(application)
 
