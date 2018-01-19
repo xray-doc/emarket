ad12c3c6e82f536abf8b27d93130d4a6ea385fe2
Mikhail Nikolaev <m.nikolaev1@gmail.com>
2018-01-19 17:02:00 +0300

    Disable whitenoise

--- first_shop/settings.py
+++ first_shop/settings.py
@@ -151,5 +151,5 @@ except:
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
