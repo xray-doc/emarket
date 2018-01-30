592e0505a2b040f4f2082b741bb242d35e95f78b
Mikhail Nikolaev <m.nikolaev1@gmail.com>
2018-01-30 11:08:42 +0300

    Added new forms in forms.py

--- first_shop/forms.py
+++ first_shop/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from products.models import Product
 
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'size': '40', 'class': 'form-control'}))
@@ -9,9 +9,22 @@ class ContactForm(forms.Form):
 
 
 class FilterProductForm(forms.Form):
+    def get_choices_from_field(field):
+        distinct_qs = Product.objects.all().values_list(field).distinct()
+        choices = [(i[0], i[0]) for i in list(distinct_qs)]  # [(2,), (4,)] > [(2,2), (4.4)]
+        choices.sort()
+        return choices
+
     os_select = forms.ChoiceField(choices=(
         ('io', 'iOS'),
         ('andr', 'Android')
     ), widget=forms.RadioSelect)
+    ram_select = forms.ChoiceField(choices=get_choices_from_field('ram'),
+                                   widget=forms.RadioSelect
+                                   )
+    diagonal_select = forms.ChoiceField(choices=get_choices_from_field('diagonal'),
+                                        widget=forms.Select
+                                        )
+    processor_select = forms.ChoiceField(choices=get_choices_from_field('processor'))
 
 
