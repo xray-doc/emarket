8779a3a03261b2617f22760008f7ea38ea8036d9
Mikhail Nikolaev <m.nikolaev1@gmail.com>
2018-03-17 15:30:03 +0300

    Revert "Fixing media files handling"
    This reverts commit 6f481f6b04d71aec7fad9d77dfa89a160c6ef9c1.

--- first_shop/settings.py
+++ first_shop/settings.py
@@ -138,8 +138,8 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/static/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
