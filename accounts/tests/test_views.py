16c2c59353ef9761f2c264578ab6359ead284044
Mikhail Nikolaev <m.nikolaev1@gmail.com>
2018-08-18 15:35:33 +0300

    Tests for login view

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -13,6 +13,36 @@ class login_viewTestCase(TestCase):
     def setUp(self):
         test_user1 = User.objects.create_user(username='testuser', password='somepass')
 
-    def test_logging_in(self):
-        response = self.client.get(reverse('accounts:register'))
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/accounts/login/')
         self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('accounts:login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('accounts:login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/form.html')
+
+    def test_registered_user(self):
+        user_obj = User.objects.get(username='testuser')
+        self.assertFalse(User.objects.get(username='testuser').last_login)
+
+        data = {'username': 'testuser', 'password': 'somepass'}
+        response = self.client.post(reverse('accounts:login'), data)
+        self.assertRedirects(response, '/', status_code=302, target_status_code=302)
+        self.assertTrue(User.objects.get(username='testuser').last_login)
+
+    def test_not_registered_user(self):
+        data = {'username': 'testusernotregistered', 'password': 'somepass'}
+        response = self.client.post(reverse('accounts:login'), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/form.html')
+
+    def test_form_is_not_valid(self):
+        data = {'username': 'testuser', 'password': ''}
+        response = self.client.post(reverse('accounts:login'), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/form.html')
